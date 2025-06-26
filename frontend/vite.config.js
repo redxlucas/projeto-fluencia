a2577a4d5c3ac49c719abb61f6d843b38fac672f
Lucas Azevedo <lucas.azevedo@dbserver.com.br>
2025-06-26 09:14:59 -0300

    hotfix: erros nosa arquivos de configuracao

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import path from "path"
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,13 +8,7 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, './src'),
+            '@': path.resolve(__dirname, './src'),
         },
     },
 })
-
-export default {
-  optimizeDeps: {
-    include: ['@radix-ui/react-hover-card'],
-  },
-};
