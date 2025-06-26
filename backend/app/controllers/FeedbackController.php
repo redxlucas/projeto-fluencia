00218080285d2d54086797af6b9747d0639e99e0
Lucas Azevedo <lucas.azevedo@dbserver.com.br>
2025-06-26 17:01:56 -0300

    fix: correção do endereco do loadenv

--- backend/app/controllers/FeedbackController.php
+++ backend/app/controllers/FeedbackController.php
@@ -3,7 +3,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require_once __DIR__ . '/LoadEnv.php';
+require_once __DIR__ . '/../helpers/LoadEnv.php';
 require_once __DIR__ . '/../helpers/Autoload.php';
 require_once __DIR__ . '/../../vendor/autoload.php';
 
